fe516c3146e6211d04aed242f107281765412a6c
Lincoln D Maene <maenedavidson@gmail.com>
2022-05-08 00:56:29 -0500

    heroku_attempt

--- MoarTrading/Code_Base/less_talking_more_trading/settings.py
+++ MoarTrading/Code_Base/less_talking_more_trading/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cusds*o61+yt3%mp$6qqzxu1&)52w9o9zik&#g+5n*%e37)q*f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['moartrading.herokuapp.com/','127.0.0.1']
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
